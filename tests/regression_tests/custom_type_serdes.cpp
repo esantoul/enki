52bc2b24b90f1b068e370288d5de218d29e5fe44
Etienne Santoul <e.santoul@gmail.com>
2024-09-16 13:07:34 +0200

    implemented new custom type serialization scheme with custom reader/writer

--- tests/regression_tests/custom_type_serdes.cpp
+++ tests/regression_tests/custom_type_serdes.cpp
@@ -59,7 +59,7 @@ namespace
   };
 } // namespace
 
-TEST_CASE("Custom Type Tagged With Member Pointers SerDes", "[regression]")
+TEST_CASE("Custom Tagged Type With Member Pointers SerDes", "[regression]")
 {
   const MyClass c1{3.14, 42};
   MyClass c2{};
@@ -111,7 +111,7 @@ namespace
   };
 } // namespace
 
-TEST_CASE("Custom Type Tagged With Bitfields SerDes", "[regression]")
+TEST_CASE("Custom Tagged Type With Bitfields SerDes", "[regression]")
 {
   static_assert(sizeof(BitStruct) == 1);
 
@@ -136,5 +136,77 @@ TEST_CASE("Custom Type Tagged With Bitfields SerDes", "[regression]")
     std::copy(std::begin(writer.data()), std::end(writer.data()), serializationOutput.begin());
     static constexpr uint32_t kExpectedSerializationData = 0x01'03'07'02;
     REQUIRE(std::bit_cast<uint32_t>(serializationOutput) == kExpectedSerializationData);
+  }
+}
+
+namespace
+{
+  struct MyStruct
+  {
+    uint8_t a : 1;
+    uint8_t b : 2;
+    uint8_t c : 3;
+    uint8_t d : 2;
+    uint8_t value;
+
+    constexpr auto operator<=>(const MyStruct &) const noexcept = default;
+  };
+
+  struct MyBinWriter : public enki::BinWriter<>
+  {
+    using enki::BinWriter<>::write;
+
+    constexpr enki::Success<void> write(const MyStruct &v)
+    {
+      const auto &[a, b, c, d, value] = v;
+      return enki::serialize(std::tie(a, b, c, d, value), *this);
+    }
+  };
+
+  struct MyBinReader : public enki::BinReader<>
+  {
+    using enki::BinReader<>::BinReader;
+    using enki::BinReader<>::read;
+
+    constexpr enki::Success<void> read(MyStruct &v)
+    {
+      std::tuple<uint8_t, uint8_t, uint8_t, uint8_t, uint8_t> data;
+      auto ret = enki::deserialize(data, *this);
+      v.a = std::get<0>(data);
+      v.b = std::get<1>(data);
+      v.c = std::get<2>(data);
+      v.d = std::get<3>(data);
+      v.value = std::get<4>(data);
+
+      return ret;
+    }
+  };
+} // namespace
+
+TEST_CASE("Custom Type With Custom Writer/Reader SerDes", "[regression]")
+{
+  const MyStruct c1{1, 2, 7, 3, 0x42};
+  MyStruct c2{};
+
+  MyBinWriter writer;
+
+  const auto serRes = enki::serialize(c1, writer);
+  REQUIRE_NOTHROW(serRes.or_throw());
+  REQUIRE(serRes.size() == 5);
+
+  const auto desRes = enki::deserialize(c2, MyBinReader(writer.data()));
+  REQUIRE_NOTHROW(desRes.or_throw());
+  REQUIRE(desRes.size() == 5);
+
+  REQUIRE(c1 == c2);
+
+  // order of registration should be preserved
+  {
+    std::array<std::byte, 5> serializationOutput;
+    std::copy(std::begin(writer.data()), std::end(writer.data()), serializationOutput.begin());
+    static constexpr std::array<uint8_t, 5> kExpectedSerializationData{
+      0x01, 0x02, 0x07, 0x03, 0x42};
+    REQUIRE(
+      std::bit_cast<std::array<uint8_t, 5>>(serializationOutput) == kExpectedSerializationData);
   }
 }
